8ffc2c821c704c70492ae702e9104917c1f86657
vuongthai1205 <giavuong.1205@gmail.com>
2024-03-18 22:34:11 +0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,3 +183,13 @@ itemMenu.forEach((e, i, parent) => {
         }
     };
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
